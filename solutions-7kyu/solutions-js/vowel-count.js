8bce360a6174cb927cffa685fb525039c1201b14
Tim Calas <timcalas@gmail.com>
2021-02-03 10:48:28 -0800

    create solutions-7kyu folder and vowel-count file

---
+++ solutions-7kyu/solutions-js/vowel-count.js
@@ -0,0 +1,15 @@
+// Vowel Count
+// Return the number (count) of vowels in the given string. 
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+    let vowelsCount = 0;
+    const vowelsArr = ['a', 'e', 'i', 'o', 'u']
+    
+    for (let char of str) {
+      if (vowelsArr.includes(char)) {
+        vowelsCount++
+      } 
+    }
+    return vowelsCount;
+  }
