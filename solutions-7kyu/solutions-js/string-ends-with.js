2ed5d13ff75b8885d0f3ff94a9238292597f7ee9
Tim Calas <timcalas@gmail.com>
2021-02-10 16:25:35 -0800

    complete new 7kyu kata

---
+++ solutions-7kyu/solutions-js/string-ends-with.js
@@ -0,0 +1,12 @@
+// String ends with?
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+function solution(str, ending) {
+    if (str.endsWith(ending)) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(solution('vader', 'er'))
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
