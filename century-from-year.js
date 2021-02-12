36866975f03c6c557ab8b1812a6142dfffce0cd3
Tim Calas <timcalas@gmail.com>
2021-02-11 22:07:18 -0800

    complete a  Math.ceil kata

---
+++ century-from-year.js
@@ -0,0 +1,6 @@
+// Century From Year
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+function century(year) {
+    return Math.ceil(year / 100)
+}
