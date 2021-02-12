10ee103541e164eff7bb55d7a882191eb264f39c
Tim Calas <timcalas@gmail.com>
2021-02-11 17:00:23 -0800

    complete 7kyu kata

---
+++ solutions-7kyu/solutions-js/middle-character.js
@@ -0,0 +1,25 @@
+// Get the Middle Character
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+function getMiddle(str) {
+    let length = str.length
+    let middle = Math.floor(length / 2)
+    
+    if (length % 2 === 0) {
+        return str[middle - 1] + str[middle]
+    } else {
+        return str[middle]
+    }
+}
+
+// function getMiddle(str) {
+//     let length = str.length
+//     let middle = Math.floor(length / 2)
+
+//     if (length % 2 === 0) {
+//         // if even
+//         return str[middle - 1] + str[middle]
+//     } else {
+//         // if odd
+//         return str[middle]
+//     }
+// }
