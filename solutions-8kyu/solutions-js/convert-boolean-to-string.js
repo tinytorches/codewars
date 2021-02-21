811930e6465488e0369e409eb2a66dcd6fb460a1
Tim Calas <timcalas@gmail.com>
2021-02-20 20:03:32 -0800

    complete kata to convert booleans to strings

---
+++ solutions-8kyu/solutions-js/convert-boolean-to-string.js
@@ -0,0 +1,9 @@
+// Convert Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+function booleanToString(boolean) {
+    if (boolean === true) {
+        return 'true'
+    } else if (boolean === false) {
+        return 'false'
+    }
+}
