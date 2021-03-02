b611be2cb3a8c761f4d8fedf6c8378ad195d0e05
Tim Calas <timcalas@gmail.com>
2021-03-02 10:22:10 -0800

    complete 5/7 katas for homework

---
+++ solutions-8kyu/solutions-js/oop-regular-ball-super-ball.js
@@ -0,0 +1,9 @@
+// Regular Ball Super Ball
+// Create a class Ball.
+// Ball objects should accept one argument for "ball type" when instantiated.
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+class Ball {
+    constructor(ballType = 'regular') {
+        this.ballType = ballType
+    }
+}
