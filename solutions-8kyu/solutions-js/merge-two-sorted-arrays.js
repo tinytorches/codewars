6cb0e8719cca42cb7d1ecfaec9c1cf13ecd6d792
Tim Calas <timcalas@gmail.com>
2021-03-17 10:50:14 -0700

    complete kata using the Set object

--- solutions-8kyu/solutions-js/merge-two-sorted-arrays.js
+++ solutions-8kyu/solutions-js/merge-two-sorted-arrays.js
@@ -3,5 +3,10 @@
 // You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 // Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
 function mergeArrays(arr1, arr2) {
-    return arr1.push(arr2)
+    // append arr2 to arr1 & sort the numbers in ascending order
+    let merge = arr1.concat(arr2).sort((a,b) => a - b)
+    // remove duplicates of numbers
+    let uniqueMerge = [...new Set(merge)]
+    // return the result
+    return uniqueMerge
 }
