c12cfebc387fffe7c6b93587dddca169f7d16f14
Tim Calas <timcalas@gmail.com>
2021-03-19 16:57:54 -0700

    complete kata and reorganize folder

---
+++ solutions-8kyu/solutions-js/convert-string-to-array.js
@@ -0,0 +1,8 @@
+// Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+function stringToArray(string) {
+    // split a string into individual words
+    // index each word into one array
+    // split returns an array
+    return array = string.split(' ')
+}
