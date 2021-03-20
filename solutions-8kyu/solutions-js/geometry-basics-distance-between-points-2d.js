dd0fbb962f16ba3901c181cebcb23bd0f06e1379
Tim Calas <timcalas@gmail.com>
2021-03-20 13:38:26 -0700

    edit to use Math.hypot

--- solutions-8kyu/solutions-js/geometry-basics-distance-between-points-2d.js
+++ solutions-8kyu/solutions-js/geometry-basics-distance-between-points-2d.js
@@ -10,3 +10,8 @@
 function distanceBetweenPoints(a, b) {
     return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y), 2))
 }
+
+// Math.hypot() is more effiecient
+function distanceBetweenPoints(a, b) {
+    return Math.hypot(a.x - b.x, a.y - b.y)
+}
