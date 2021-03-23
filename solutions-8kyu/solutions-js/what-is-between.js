aa0bf0324a0d9bb60e029c7177877b152dd71f71
Tim Calas <timcalas@gmail.com>
2021-03-22 21:18:00 -0700

    failed initial attempt 😁

---
+++ solutions-8kyu/solutions-js/what-is-between.js
@@ -0,0 +1,11 @@
+// What is between?
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// initial attempt
+function between(a, b) {
+    let arr = [];
+    for (var i = a; i <= b; i++) {
+        return arr.push(i);
+    }
+}
+
