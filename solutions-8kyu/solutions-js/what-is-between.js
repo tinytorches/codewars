47218795c4d6861e6d807d8651acc87787249732
Tim Calas <timcalas@gmail.com>
2021-03-22 21:35:00 -0700

    complete kata, fixed simple error

--- solutions-8kyu/solutions-js/what-is-between.js
+++ solutions-8kyu/solutions-js/what-is-between.js
@@ -1,13 +1,9 @@
 // What is between?
 // Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
-
-// initial attempt
 function between(a, b) {
     let arr = [];
     for (let i = a; i <= b; i++) {
-        return arr.push(i);
+        arr.push(i);
     }
+    return arr
 }
-
-// Array.from() attempt
-const between = (a, b) => Array.from({length: (b - a)})
