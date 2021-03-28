cccab915e4bd55c37c9b599097eaa07af98f41a6
Tim Calas <timcalas@gmail.com>
2021-03-27 22:35:28 -0700

    complete kata

--- solutions-8kyu/solutions-js/nth-even-number.js
+++ solutions-8kyu/solutions-js/nth-even-number.js
@@ -1,10 +1,2 @@
 // Get the Nth Even Number
-function nthEven(n) {
-    let result = 0
-    let let j = 0
-
-    for ( let i = 0; i < n; i++ ) {
-        j += 2
-    }
-    return (n * 2) - 2 
-}
+const nthEven = n => (n * 2) - 2 
