89ea11946bfaa6ee9bf81e5f4dd4abc1a9cb1349
Tim Calas <timcalas@gmail.com>
2021-04-05 20:27:29 -0700

    complete kata

---
+++ solutions-8kyu/solutions-js/filter-out-geese.js
@@ -0,0 +1,10 @@
+// Filter out the geese
+// Write a function, gooseFilter / goose-filter / goose_filter / GooseFilter, that takes an array of strings as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+function gooseFilter(birds) {
+    let geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'];
+
+    let noGeeseAllowed = birds.filter(goose => !geese.includes(goose));
+
+    return noGeeseAllowed;
+}
