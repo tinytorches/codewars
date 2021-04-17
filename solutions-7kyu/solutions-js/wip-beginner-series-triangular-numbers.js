d85ccbf5979505fd7294283a9b242c2079dc7e84
Tim Calas <timcalas@gmail.com>
2021-04-16 17:54:56 -0700

    rename kata and started kata that is now in-progress

---
+++ solutions-7kyu/solutions-js/wip-beginner-series-triangular-numbers.js
@@ -0,0 +1,20 @@
+// Beginner Series #5 Triangular Numbers
+// Triangular number is the amount of points that can fill equilateral triangle. 
+// Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
+// Given a number 'T' from interval [1; 2147483646], find if it is triangular number or not.
+
+// Hint!
+// T(n) = n * (n + 1) / 2,
+// n - is the size of one side.
+// T(n) - is the triangular number.
+
+const isTriangular = (t) => {
+    let 
+}
+
+// each following row increases by 1
+// have to have the ability to measure each side separately and compare
+// if equal, return true
+
+
+// loop
