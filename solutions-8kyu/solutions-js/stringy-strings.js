2a5004566918ce977b892f8f4d36b497b887cfa9
Tim Calas <timcalas@gmail.com>
2021-04-23 10:11:33 -0700

    complete strings kata

---
+++ solutions-8kyu/solutions-js/stringy-strings.js
@@ -0,0 +1,6 @@
+// Stringy Strings
+// write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+const stringy = (size) => {
+    return ''.padStart(size, '10')
+}
