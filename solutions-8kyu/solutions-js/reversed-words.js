9632654ef6fd8bc2d081d72de110b4d7e90754ce
Tim Calas <timcalas@gmail.com>
2021-04-23 16:10:16 -0700

    complete strings kata

---
+++ solutions-8kyu/solutions-js/reversed-words.js
@@ -0,0 +1,6 @@
+// Reversed Words
+// Complete the solution so that it reverses all of the words within the string passed in. 
+
+const reverseWords = (str) => {
+    return str.split(' ').reverse().join(' ')
+}
