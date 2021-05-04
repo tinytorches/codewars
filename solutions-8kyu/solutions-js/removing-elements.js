006e52f5451311e75409b0c199661192787635d5
Tim Calas <timcalas@gmail.com>
2021-05-04 12:24:32 -0700

    complete arrays kata

---
+++ solutions-8kyu/solutions-js/removing-elements.js
@@ -0,0 +1,7 @@
+// Removing Elements
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Initial solution
+const removeEveryOther = (arr) => {
+    return arr.filter((a, i) => i % 2 === 0)
+}
