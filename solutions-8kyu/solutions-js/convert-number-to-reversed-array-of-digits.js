44cd3131c844f5feb84c05fd439553a90a466697
Tim Calas <timcalas@gmail.com>
2021-05-07 17:59:18 -0700

    complete arrays kata

---
+++ solutions-8kyu/solutions-js/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,11 @@
+// Convert number to reversed array of digits
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example:
+// 348597 => [7,9,5,8,4,3]
+
+// Initial solution
+const digitize = (n) => {
+    return Array.from(String(n), Number).reverse()
+}
