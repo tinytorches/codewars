764b876cbe9273cf79d5549424b0d5af67da421a
Tim Calas <timcalas@gmail.com>
2021-05-12 15:08:25 -0700

    complete fundamentals kata

---
+++ solutions-8kyu/solutions-js/grasshopper-debug-sayHello.js
@@ -0,0 +1,11 @@
+// Grasshopper - Debug sayHello
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/javascript
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// Example output: 
+// Hello, Mr. Spock
+
+const sayHello = (name) => {
+    return `Hello, ${name}`
+}
