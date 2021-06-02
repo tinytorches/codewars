14ecd6581ad05c7198eacd0f216a0039c0954ef8
Tim Calas <timcalas@gmail.com>
2021-06-01 20:51:54 -0700

    complete kata

---
+++ solutions-8kyu/solutions-js/function-3-multiplying-two-numbers.js
@@ -0,0 +1,8 @@
+// Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+// Implement a function which multiplies two numbers.
+
+const multiply = ( num1, num2) => {
+    return num1 * num2
+}
