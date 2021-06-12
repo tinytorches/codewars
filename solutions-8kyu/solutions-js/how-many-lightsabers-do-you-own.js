b2de858f0448f70373565cd4ef1944aabbda8d65
Tim Calas <timcalas@gmail.com>
2021-06-11 23:02:35 -0700

    complete kata

---
+++ solutions-8kyu/solutions-js/how-many-lightsabers-do-you-own.js
@@ -0,0 +1,21 @@
+// How many lightsabers do you own?
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
+
+// Inspired by the development team at Vooza, write the function thataccepts the name of a programmer, and returns the number of lightsabers owned by that person.
+
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+const howManyLightsabersDoYouOwn = ( name ) => {
+    if ( name !== 'Zach' ) {
+        return 0
+    } else {
+        return 18
+    }
+}
+
+// Alternative solution
+const howManyLightsabersDoYouOwn = ( name ) => {
+    return name === 'Zach' ? 18 : 0
+}
