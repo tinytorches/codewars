a3e0711adff072555d4b3ac79a08775d52632b69
Tim Calas <timcalas@gmail.com>
2021-06-18 21:32:18 -0700

    complete kata

---
+++ solutions-8kyu/solutions-js/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,8 @@
+// Grasshopper - Function syntax debugging
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+function main ( verb, noun ) {
+    return verb + noun
+}
---
+++ solutions-8kyu/solutions-js/grasshopper-terminal-game-combat-function.js
@@ -0,0 +1,13 @@
+// Grasshopper - Terminal game combat function
+// https://www.codewars.com/kata/586c1cf4b98de0399300001d
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat ( health, damage ) {
+    let newHealth = health - damage
+    if ( newHealth < 0 ) {
+        return 0
+    } else {
+        return newHealth
+    }
+}
