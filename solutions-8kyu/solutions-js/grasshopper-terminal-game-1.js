1add3927f601779aaed60903dfbec7e1e2ddb628
Tim Calas <timcalas@gmail.com>
2021-06-22 21:49:55 -0700

    complete kata

---
+++ solutions-8kyu/solutions-js/grasshopper-terminal-game-1.js
@@ -0,0 +1,29 @@
+// Grasshopper - Terminal Game #1
+// https://www.codewars.com/kata/55e8aba23d399a59500000ce
+
+// In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+// attribute 	value
+// name 	user argument or 'Hero'
+// position 	'00'
+// health 	100
+// damage 	5
+// experience 	0
+
+function Hero (name = 'Hero', position, health, damage, experience) {
+    this.name = name
+    this.position =  '00'
+    this.health = 100
+    this.damage = 5
+    this.experience = 0
+}
+
+// Alternative solution:
+class Hero {
+    constructor ( name = 'Hero' ) {
+        this.name = name
+        this.position = '00'
+        this.health = 100
+        this.damage = 5
+        this.experience = 0
+    }
+}
